e9533639cb9d9fc23877b92e964377e32970ef4c
jabir254 <jabirkullow@gmail.com>
2023-02-09 21:39:33 +0300

    updated

--- models.py
+++ models.py
@@ -1,7 +1,7 @@
 #models.py
 
 from datetime import datetime
-from config import db
+from config import db,ma
 
 class Person(db.Model):
     __tablename__ = 'person'
@@ -9,3 +9,12 @@ class Person(db.Model):
     lname = db.column(db.String(32), unique=True)
     fname = db.column(db.String(32))
     timestamp = db.column(db.Datertime, default=datetime.utcnow, onupdate= datetime.utcnow)
+
+class PersonSchema(ma.SQLAlchemyAutoSchema):
+    class Meta:
+        model = Person
+        load_instance =True
+        sqla_session = db.session
+
+person_schema= PersonSchema()
+people_schema= PersonSchema(many=True)
